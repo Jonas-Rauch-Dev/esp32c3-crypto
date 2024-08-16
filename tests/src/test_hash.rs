bf1ace72d72a04a749136551087782e7067ed8ee
Jonas <jonasrauch23@gmail.com>
2024-08-16 09:46:41 +0200

    test: more hash cases

--- tests/src/test_hash.rs
+++ tests/src/test_hash.rs
@@ -20,6 +20,19 @@ pub fn test_hash() {
     }
 }
 
+fn test_hash_function(data: &[u8], out: &mut [u8], expected: &[u8], hash: &mut Hash) -> Result<(), &'static str> {
+    if let Ok(result) = hash.hash(data, out) {
+        if result != expected {
+            error!("The result ({:?}) does not equal the expected_result ({:?})", result, expected);
+            return Err("Hash results do not match");
+        }
+    } else {
+        return Err("Error while hashing");
+    }
+
+    Ok(())
+}
+
 
 fn test_sha1() -> Result<(), &'static str>{
     let sha = unsafe { Peripherals::steal().SHA };
@@ -38,26 +51,25 @@ fn test_sha1() -> Result<(), &'static str>{
     }
 
 
-    let mut out = [0; 1];
-    let data = [0; 1];
-
-    if let Ok(_) = hash.hash(&data, &mut out) {
+    if let Ok(_) = hash.hash(&[0; 1], &mut [0; 1]) {
         return Err("Should error with undersized out");
     }
 
+    // Test output with exact size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 20],
+        &[0x38, 0x66, 0x6b, 0x8b, 0xa5, 0x00, 0xfa, 0xa5, 0xc2, 0x40, 0x6f, 0x45, 0x75, 0xd4, 0x2a, 0x92, 0x37, 0x98, 0x44, 0xc2],
+        &mut hash
+    )?;
 
-    let mut out = [0; 20];
-    let data = "aaaaaaaaaaaaaaaaaaaa";
-
-    if let Ok(result) = hash.hash(data.as_bytes(), &mut out) {
-        let expected_result = [0x38, 0x66, 0x6b, 0x8b, 0xa5, 0x00, 0xfa, 0xa5, 0xc2, 0x40, 0x6f, 0x45, 0x75, 0xd4, 0x2a, 0x92, 0x37, 0x98, 0x44, 0xc2];
-        if result != expected_result {
-            error!("The result ({:?}) does not equal the expected_result ({:?})", result, expected_result);
-            return Err("Hash results do not match");
-        }
-    } else {
-        return Err("Error while hashing");
-    }
+    // Test output with bigger size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 40],
+        &[0x38, 0x66, 0x6b, 0x8b, 0xa5, 0x00, 0xfa, 0xa5, 0xc2, 0x40, 0x6f, 0x45, 0x75, 0xd4, 0x2a, 0x92, 0x37, 0x98, 0x44, 0xc2],
+        &mut hash
+    )?;
 
     Ok(())
 }
@@ -87,19 +99,21 @@ fn test_sha224() -> Result<(), &'static str>{
         return Err("Should error with undersized out");
     }
 
+    // Test output with exact size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 28],
+        &[0x43, 0x58, 0x6e, 0xff, 0x52, 0xcb, 0xaf, 0x9f, 0x22, 0x48, 0x2f, 0x34, 0xa9, 0x43, 0x7f, 0xf4, 0x5b, 0xd2, 0xe7, 0x31, 0x2a, 0xd5, 0x86, 0xb3, 0xdd, 0x82, 0x80, 0x2f],
+        &mut hash
+    )?;
 
-    let mut out = [0; 28];
-    let data = "aaaaaaaaaaaaaaaaaaaa";
-
-    if let Ok(result) = hash.hash(data.as_bytes(), &mut out) {
-        let expected_result = [0x43, 0x58, 0x6e, 0xff, 0x52, 0xcb, 0xaf, 0x9f, 0x22, 0x48, 0x2f, 0x34, 0xa9, 0x43, 0x7f, 0xf4, 0x5b, 0xd2, 0xe7, 0x31, 0x2a, 0xd5, 0x86, 0xb3, 0xdd, 0x82, 0x80, 0x2f];
-        if result != expected_result {
-            error!("The result ({:?}) does not equal the expected_result ({:?})", result, expected_result);
-            return Err("Hash results do not match");
-        }
-    } else {
-        return Err("Error while hashing");
-    }
+    // Test output with bigger size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 44],
+        &[0x43, 0x58, 0x6e, 0xff, 0x52, 0xcb, 0xaf, 0x9f, 0x22, 0x48, 0x2f, 0x34, 0xa9, 0x43, 0x7f, 0xf4, 0x5b, 0xd2, 0xe7, 0x31, 0x2a, 0xd5, 0x86, 0xb3, 0xdd, 0x82, 0x80, 0x2f],
+        &mut hash
+    )?;
 
     Ok(())
 }
@@ -129,19 +143,21 @@ fn test_sha256() -> Result<(), &'static str>{
         return Err("Should error with undersized out");
     }
 
+    // Test output with exact size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 32],
+        &[0x42, 0x49, 0x2d, 0xa0, 0x62, 0x34, 0xad, 0x0a, 0xc7, 0x6f, 0x5d, 0x5d, 0xeb, 0xdb, 0x6d, 0x1a, 0xe0, 0x27, 0xcf, 0xfb, 0xe7, 0x46, 0xa1, 0xc1, 0x3b, 0x89, 0xbb, 0x8b, 0xc0, 0x13, 0x91, 0x37],
+        &mut hash
+    )?;
 
-    let mut out = [0; 32];
-    let data = "aaaaaaaaaaaaaaaaaaaa";
-
-    if let Ok(result) = hash.hash(data.as_bytes(), &mut out) {
-        let expected_result = [0x42, 0x49, 0x2d, 0xa0, 0x62, 0x34, 0xad, 0x0a, 0xc7, 0x6f, 0x5d, 0x5d, 0xeb, 0xdb, 0x6d, 0x1a, 0xe0, 0x27, 0xcf, 0xfb, 0xe7, 0x46, 0xa1, 0xc1, 0x3b, 0x89, 0xbb, 0x8b, 0xc0, 0x13, 0x91, 0x37];
-        if result != expected_result {
-            error!("The result ({:?}) does not equal the expected_result ({:?})", result, expected_result);
-            return Err("Hash results do not match");
-        }
-    } else {
-        return Err("Error while hashing");
-    }
+    // Test output with bigger size
+    test_hash_function(
+        "aaaaaaaaaaaaaaaaaaaa".as_bytes(),
+        &mut [0; 44],
+        &[0x42, 0x49, 0x2d, 0xa0, 0x62, 0x34, 0xad, 0x0a, 0xc7, 0x6f, 0x5d, 0x5d, 0xeb, 0xdb, 0x6d, 0x1a, 0xe0, 0x27, 0xcf, 0xfb, 0xe7, 0x46, 0xa1, 0xc1, 0x3b, 0x89, 0xbb, 0x8b, 0xc0, 0x13, 0x91, 0x37],
+        &mut hash
+    )?;
 
     Ok(())
 }
